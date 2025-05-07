ae53698976112099198094d0d6b6df0c3de4e718
deepeshJCU <deepeshbijarnia2911@gmail.com>
2025-05-08 00:28:07 +0100

    Fix Admin Settings

--- wp-course-recommender/admin/admin-ui.php
+++ wp-course-recommender/admin/admin-ui.php
@@ -10,11 +10,37 @@ function cr_admin_settings_page() {
         <h1>Course Recommender Settings</h1>
         <form method="post" action="options.php">
             <?php
-            settings_fields('cr_settings_group');
-            do_settings_sections('cr-settings');
+            settings_fields('course_recommender_settings_group');
+            do_settings_sections('course-recommender');
             submit_button();
             ?>
         </form>
     </div>
     <?php
+}
+
+add_action('admin_init', 'course_recommender_register_settings');
+
+function course_recommender_register_settings() {
+    register_setting('course_recommender_settings_group', 'course_recommender_interests');
+
+    add_settings_section(
+        'course_recommender_main_section',
+        'General Settings',
+        null,
+        'course-recommender'
+    );
+
+    add_settings_field(
+        'course_recommender_interests',
+        'Default Interests (comma separated)',
+        'course_recommender_interests_callback',
+        'course-recommender',
+        'course_recommender_main_section'
+    );
+}
+
+function course_recommender_interests_callback() {
+    $value = get_option('course_recommender_interests', '');
+    echo '<input type="text" name="course_recommender_interests" value="' . esc_attr($value) . '" class="regular-text">';
 }
